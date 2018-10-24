a3478a532506cbb46937914531ec529112f4fa75
YoshinoMizuyama <yoshinon.non.1008@gmail.com>
2018-10-24 15:57:17 +0900

    my first commit

---
+++ test10.py
@@ -0,0 +1,45 @@
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "__________",
+              "|         ",
+              "|    |    ",
+              "|    0    ",
+              "|   /|\   ",
+              "|   / \   ",
+              "          "]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("Welcome to hangman !!")
+
+# (1)
+    while wrong < len(stages)-1:
+        print("\n")
+        msg = "guess a word:"
+        char = input(msg)
+# (2)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = "$"
+        else:
+            wrong += 1
+# (3)
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+#(4)
+        if "_" not in board:
+            print("You Win !!")
+            print(" ".join(board))
+            win = True
+            break
+
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("You Lose !!")
+
+
+if __name__ == '__main__':
+    hangman("aaachan")
